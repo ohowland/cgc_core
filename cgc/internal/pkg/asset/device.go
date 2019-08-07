52534b79205d3676ceb82dd206d2191cae61dd74
owen <ohowland@gmail.com>
2019-08-07 11:20:32 -0600

    basic scheduler and actor

---
+++ cgc/internal/pkg/asset/device.go
@@ -0,0 +1,9 @@
+package asset
+
+// Device is the interface to read/write a physical component.
+// this type of read write will almost certianly have some latency associated with it
+// and should not be done as a blocking operation
+type Device interface {
+	ReadDeviceStatus() error
+	WriteDeviceControl() error
+}
