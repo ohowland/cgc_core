04af05bc7ecee9a91cacfc7e1ffcf9c137388947
owen <ohowland@gmail.com>
2019-12-07 14:53:10 -0700

    update

--- internal/pkg/dispatch/manualdispatch/manualdispatch.go
+++ internal/pkg/dispatch/manualdispatch/manualdispatch.go
@@ -1,4 +1,26 @@
 package manualdispatch
 
-type Dispatch struct {
+import (
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/ohowland/cgc/internal/pkg/dispatch"
+)
+
+type Core struct {
+	mux              *sync.Mutex
+	calculatedStatus dispatch.CalculatedStatus
+	assetState       map[uuid.UUID]interface{}
+}
+
+func (c Core) UpdateStatus(uuid.UUID, interface{}) {
+
+}
+
+func (c *Core) DropStatus(uuid.UUID) {
+
+}
+
+func (c Core) GetControl() map[uuid.UUID]interface{} {
+	return c.assetState
 }
